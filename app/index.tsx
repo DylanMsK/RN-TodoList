378e536b0a4a8fc88df41b8ef01fb9d3cfc03252
dylanmsk <kms920612@gmail.com>
2025-01-03 17:25:29 +0900

    todo 추가 로직까지 완료

--- app/index.tsx
+++ app/index.tsx
@@ -4,30 +4,62 @@ import {
   View,
   StyleSheet,
   TouchableOpacity,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
+  TextInput,
 } from "react-native";
+import { useState } from "react";
 
 export default function Index() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState({});
+  // toDos[Date.now()] = {
+  //   text: "record",
+  //   work: true,
+  // };
+
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (text: string) => setText(text);
+  const addTodo = () => {
+    if (text === "") {
+      return;
+    }
+    // save to do
+    const newTodos = Object.assign({}, todos, {
+      [Date.now()]: { text, work: working },
+    });
+    setTodos(newTodos);
+    setText("");
+  };
+  console.log(todos);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity
-          // activeOpacity={1}
-          onPress={() => {
-            console.log("Work");
-          }}
-        >
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
-        <TouchableWithoutFeedback
-          onPress={() => {
-            console.log("Travel");
-          }}
-        >
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableWithoutFeedback>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{ ...styles.btnText, color: working ? theme.grey : "white" }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
       </View>
+
+      <TextInput
+        onChangeText={onChangeText}
+        onSubmitEditing={addTodo}
+        returnKeyType="done"
+        value={text}
+        placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+        style={styles.input}
+      />
     </View>
   );
 }
@@ -44,9 +76,16 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
   },
   btnText: {
-    // color: theme.grey,
     color: "white",
     fontSize: 38,
     fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
